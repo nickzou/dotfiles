430601de2208ff015d71ee9e390f66931f80f68f
Nick Zou <z.nick00@gmail.com>
2024-12-30 00:47:16 -0500

    add lua lsp

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -112,5 +112,28 @@ return {
     };
   };
 }
+require('lspconfig').lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false, -- Disable third party checking
+      },
+      -- Do not send telemetry data
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
     end
 }
