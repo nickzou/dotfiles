968da6ff4a12941e8b58355806f2bb7d77a20be1
Nick Zou <z.nick00@gmail.com>
2025-01-01 11:15:04 -0500

    add wordpress related stubs

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -32,108 +32,114 @@ return {
 	           }
 	       })
 	require('lspconfig').intelephense.setup{
-  settings = {
-    intelephense = {
-      stubs = { 
-        "apache",
-        "bcmath",
-        "bz2",
-        "calendar",
-        "com_dotnet",
-        "Core",
-        "curl",
-        "date",
-        "dba",
-        "dom",
-        "enchant",
-        "exif",
-        "FFI",
-        "fileinfo",
-        "filter",
-        "fpm",
-        "ftp",
-        "gd",
-        "gettext",
-        "gmp",
-        "hash",
-        "iconv",
-        "imap",
-        "intl",
-        "json",
-        "ldap",
-        "libxml",
-        "mbstring",
-        "meta",
-        "mysqli",
-        "oci8",
-        "odbc",
-        "openssl",
-        "pcntl",
-        "pcre",
-        "PDO",
-        "pdo_ibm",
-        "pdo_mysql",
-        "pdo_pgsql",
-        "pdo_sqlite",
-        "pgsql",
-        "Phar",
-        "posix",
-        "pspell",
-        "readline",
-        "Reflection",
-        "session",
-        "shmop",
-        "SimpleXML",
-        "snmp",
-        "soap",
-        "sockets",
-        "sodium",
-        "SPL",
-        "sqlite3",
-        "standard",
-        "superglobals",
-        "sysvmsg",
-        "sysvsem",
-        "sysvshm",
-        "tidy",
-        "tokenizer",
-        "xml",
-        "xmlreader",
-        "xmlrpc",
-        "xmlwriter",
-        "xsl",
-        "Zend OPcache",
-        "zip",
-        "zlib"
-      },
-      files = {
-        maxSize = 5000000;
-      };
-    };
-  };
-}
-require('lspconfig').lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false, -- Disable third party checking
-      },
-      -- Do not send telemetry data
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+	  settings = {
+	    intelephense = {
+	      stubs = { 
+		"apache",
+		"bcmath",
+		"bz2",
+		"calendar",
+		"com_dotnet",
+		"Core",
+		"curl",
+		"date",
+		"dba",
+		"dom",
+		"enchant",
+		"exif",
+		"FFI",
+		"fileinfo",
+		"filter",
+		"fpm",
+		"ftp",
+		"gd",
+		"gettext",
+		"gmp",
+		"hash",
+		"iconv",
+		"imap",
+		"intl",
+		"json",
+		"ldap",
+		"libxml",
+		"mbstring",
+		"meta",
+		"mysqli",
+		"oci8",
+		"odbc",
+		"openssl",
+		"pcntl",
+		"pcre",
+		"PDO",
+		"pdo_ibm",
+		"pdo_mysql",
+		"pdo_pgsql",
+		"pdo_sqlite",
+		"pgsql",
+		"Phar",
+		"posix",
+		"pspell",
+		"readline",
+		"Reflection",
+		"session",
+		"shmop",
+		"SimpleXML",
+		"snmp",
+		"soap",
+		"sockets",
+		"sodium",
+		"SPL",
+		"sqlite3",
+		"standard",
+		"superglobals",
+		"sysvmsg",
+		"sysvsem",
+		"sysvshm",
+		"tidy",
+		"tokenizer",
+		"xml",
+		"xmlreader",
+		"xmlrpc",
+		"xmlwriter",
+		"xsl",
+		"Zend OPcache",
+		"zip",
+		"zlib",
+		"wordpress",
+                "woocommerce",
+                "acf-pro",
+                "acf-stubs",
+                "wordpress-globals",
+                "wp-cli",
+	      },
+	      files = {
+		maxSize = 5000000;
+	      };
+	    };
+	  };
+	}
+	require('lspconfig').lua_ls.setup {
+	  settings = {
+	    Lua = {
+	      runtime = {
+		-- Tell the language server which version of Lua you're using
+		version = 'LuaJIT',
+	      },
+	      diagnostics = {
+		-- Get the language server to recognize the `vim` global
+		globals = {'vim'},
+	      },
+	      workspace = {
+		-- Make the server aware of Neovim runtime files
+		library = vim.api.nvim_get_runtime_file("", true),
+		checkThirdParty = false, -- Disable third party checking
+	      },
+	      -- Do not send telemetry data
+	      telemetry = {
+		enable = false,
+	      },
+	    },
+	  },
+	}
     end
 }
