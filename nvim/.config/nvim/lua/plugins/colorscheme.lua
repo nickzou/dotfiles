1c15c71b3a81c49c8f10a234cda82c096669a069
Nick Zou <z.nick00@gmail.com>
2025-01-08 23:27:53 -0500

    change theme to carbonfox

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,19 @@
-return { "EdenEast/nightfox.nvim" } 
+return {
+  "EdenEast/nightfox.nvim",
+  lazy = false, -- make sure this loads during startup
+  priority = 1000, -- load this before other plugins
+  config = function()
+    require("nightfox").setup({
+      options = {
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    })
+    vim.cmd("colorscheme carbonfox")
+  end,
+}
