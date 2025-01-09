4087d93cc37f13b02c8731fc55a4e72e1ad62273
Nick <z.nick00@gmail.com>
2025-01-09 15:48:12 -0500

    change highlight color

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,11 @@ return {
   priority = 1000, -- load this before other plugins
   config = function()
     require("nightfox").setup({
+	palettes = {
+		carbonfox = {
+			sel0="#4b4b4b"
+		}
+	},
       options = {
         transparent = false,
         terminal_colors = true,
