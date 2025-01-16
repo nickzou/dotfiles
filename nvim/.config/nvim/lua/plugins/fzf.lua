238361bc4a283f42834ec35e8c5f8c1f0f38b610
Nick Zou <z.nick00@gmail.com>
2025-01-16 00:12:53 -0500

    add visual grep keymap

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -6,7 +6,8 @@ return {
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
 	vim.keymap.set("n", "<space>p", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true }),
-	vim.keymap.set("n", "<space>f", "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "<space>ff", "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true }),
+	vim.keymap.set("v", "<space>fg", "<cmd>lua require('fzf-lua').grep_visual()<CR>", { noremap = true, silent = true })
     })
   end
 }
