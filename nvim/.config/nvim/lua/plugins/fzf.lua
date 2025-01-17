512ceb4159891213572bdfaa3e3f0a2eee3748ab
Nick Zou <z.nick00@gmail.com>
2025-01-17 10:36:11 -0500

    fix spacing

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -5,9 +5,12 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
+	  grep = {
+	    rg_opts = "--column --line-number --no-heading --color=always --smart-case --hidden --glob '!{node_modules,vendor}/*' --glob '!.git/*'"
+	  },
+	})
 	vim.keymap.set("n", "<space>p", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true }),
 	vim.keymap.set("n", "<space>ff", "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true }),
 	vim.keymap.set("v", "<space>fg", "<cmd>lua require('fzf-lua').grep_visual()<CR>", { noremap = true, silent = true })
-    })
   end
 }
