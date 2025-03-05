36792abd4045ef8f2d00839a6a3a98d912c1d029
Nick Zou <z.nick00@gmail.com>
2025-03-05 18:56:07 -0500

    fix config issue

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -6,16 +6,9 @@ return {
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
 	  grep = {
-                rg_opts = [[
-                    --column
-                    --line-number
-                    --no-heading
-                    --color=always
-                    --smart-case
-                    --hidden
-                    --glob=!{node_modules,vendor,.git,.idea}/*
-                    --glob=!*.{lock,svg,png}
-                ]]
+            rg_opts = "--column --line-number --no-heading --color=always --smart-case --hidden "..
+              "--glob=!node_modules/* --glob=!vendor/* --glob=!.git/* "..
+              "--glob=!*.lock --glob=!*.svg --glob=!*.png"
 	  },
 	})
 	vim.keymap.set("n", "<space>p", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
