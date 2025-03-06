10f788b3df48eed1462b61123031dbf59aa6782f
Nick Zou <z.nick00@gmail.com>
2025-03-05 21:36:19 -0500

    add php block keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,24 @@ vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>hp', 'a<?php  ?><Left><Left><Left>', { noremap = true })
 vim.keymap.set('n', '<leader>hs', 'a<?=  ?><Left><Left><Left>', { noremap = true })
+vim.keymap.set('n', '<leader>hP', function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local line = vim.api.nvim_get_current_line()
+    local row, col = pos[1], pos[2]
+    
+    -- Get indentation of current line
+    local indent = line:match("^%s*")
+    
+    -- Insert the PHP tags with an empty line between them
+    vim.api.nvim_buf_set_lines(0, row-1, row, false, {
+        indent .. "<?php",
+        indent,
+        indent .. "?>"
+    })
+    
+    -- Position cursor on the empty line
+    vim.api.nvim_win_set_cursor(0, {row+1, #indent})
+    
+    -- Enter insert mode
+    vim.cmd('startinsert')
+end, { noremap = true })
