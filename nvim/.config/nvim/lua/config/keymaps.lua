2dba695ba5ac67b52374d874818e9b8144224aaf
Nick Zou <z.nick00@gmail.com>
2025-03-14 13:58:50 -0400

    change tree plugin

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>rn', ':set relativenumber!<CR>')
-vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', '<leader>tt', ':NvimTreeFocus<CR>')
+vim.keymap.set('n', '<leader>t', ':Neotree toggle reveal <CR>')
 vim.keymap.set('n', '<leader>gsd<CR>', ':Gitsigns reset_hunk<CR>')
 vim.keymap.set('n', '<leader>gss<CR>', ':Gitsigns stage_hunk<CR>')
 vim.keymap.set('n', '<leader>gs', ':Git status<CR>')
--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -1,11 +1,22 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   lazy = false,
   dependencies = {
+    "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
+    },
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("neo-tree").setup {}
   end,
 }
