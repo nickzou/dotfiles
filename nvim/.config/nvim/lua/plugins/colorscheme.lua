912bb840e52e68c7dbe8436f2c96159872564793
Nick Zou <z.nick00@gmail.com>
2025-03-14 10:46:54 -0400

    update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,8 @@
 return {
-  "EdenEast/nightfox.nvim",
-  lazy = false, -- make sure this loads during startup
-  priority = 1000, -- load this before other plugins
-  config = function()
-    require("nightfox").setup({
-	palettes = {
-		carbonfox = {
-			sel0="#4b4b4b"
-		}
-	},
-      options = {
-        transparent = false,
-        terminal_colors = true,
-        styles = {
-          comments = "italic",
-          keywords = "bold",
-          types = "italic,bold",
-        },
-      },
-    })
-    vim.cmd("colorscheme carbonfox")
-  end,
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("cyberdream") 
+    end,
 }
