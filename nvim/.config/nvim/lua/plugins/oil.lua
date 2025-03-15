65295bf862d88e519e6c1482e054101d7ccda825
Nick Zou <z.nick00@gmail.com>
2025-03-15 01:26:14 -0400

    make default file explorer

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,4 +7,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function()
+      require("oil").setup({
+          default_file_explorer = true,
+      })
+  end,
 }
