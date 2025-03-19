33955ce151d478ca1fa5bd4585e6dc83fbd45e4d
Nick Zou <z.nick00@gmail.com>
2025-03-18 20:24:16 -0400

    add custom color for FlashLabel

---
+++ nvim/.config/nvim/lua/config/colors.lua
@@ -0,0 +1,22 @@
+-- colors.lua - Custom highlight for Flash.nvim match
+
+-- Create an autocommand group for our highlight override
+local highlight_group = vim.api.nvim_create_augroup("CustomHighlights", { clear = true })
+
+-- Set up highlight that persists across colorscheme changes
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = highlight_group,
+  callback = function()
+    -- Only customize FlashMatch
+    vim.api.nvim_set_hl(0, "FlashLabel", {
+      fg = "#e30909",
+      bg = "#d1cfcf",
+      bold = true
+    })
+  end,
+})
+
+-- Trigger the highlight immediately upon loading this file
+vim.cmd("colorscheme " .. vim.g.colors_name)
+
+return {}
--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,19 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+      highlight = {
+        backdrop = true,
+        matches = true,
+        priority = 5000,
+        groups = {
+          match = "FlashMatch",
+          current = "FlashCurrent",
+          backdrop = "FlashBackdrop",
+          label = "FlashLabel",
+        },
+      },
+  },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
