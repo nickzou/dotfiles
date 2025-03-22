7b4a3b8b72682077a5505e2efe3148dcff19d879
Nick Zou <z.nick00@gmail.com>
2025-03-22 00:27:52 -0400

    update emmet mode to all

--- nvim/.config/nvim/lua/plugins/emmet.lua
+++ nvim/.config/nvim/lua/plugins/emmet.lua
@@ -2,8 +2,7 @@ return {
     'mattn/emmet-vim',
     ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'blade', 'twig', 'php' },
     init = function()
-        vim.g.user_emmet_mode = 'n'
+        vim.g.user_emmet_mode = 'a'
         vim.g.user_emmet_leader_key = '<C-y>'
     end,
-
 }
