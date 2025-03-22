a353ca4588ae9728b38a744815b00b7b23eb416a
Nick Zou <z.nick00@gmail.com>
2025-03-22 00:04:44 -0400

    add init function

--- nvim/.config/nvim/lua/plugins/emmet.lua
+++ nvim/.config/nvim/lua/plugins/emmet.lua
@@ -1,4 +1,9 @@
 return {
     'mattn/emmet-vim',
-    ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'blade', 'twig', 'php' }
+    ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'blade', 'twig', 'php' },
+    init = function()
+        vim.g.user_emmet_mode = 'n'
+        vim.g.user_emmet_leader_key = '<C-y>'
+    end,
+
 }
