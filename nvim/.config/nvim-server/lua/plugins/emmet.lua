24fefa1453a1d2edcee5deae0d5bef4b80d9fc69
Nick Zou <z.nick00@gmail.com>
2025-05-16 23:44:47 -0400

    remove heavy plugins

--- nvim/.config/nvim-server/lua/plugins/emmet.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'mattn/emmet-vim',
-    ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'blade', 'twig', 'php' },
-    init = function()
-        vim.g.user_emmet_mode = 'a'
-        vim.g.user_emmet_leader_key = '<C-y>'
-    end,
-}
