2fbe1697a4d9d9d5a586a1a789e805e23a9b7a1b
Nick Zou <z.nick00@gmail.com>
2025-05-30 15:46:35 -0400

    add bounce

---
+++ nvim/.config/nvim/lua/plugins/bounce.lua
@@ -0,0 +1,10 @@
+  return {
+    'R1PeR/bounce.nvim',
+    config = function()
+      require('bounce').setup {
+        delay_time = 1000,
+        highlight_group_name = '@text.todo',
+        more_jumps = false,
+      }
+    end,
+  }
