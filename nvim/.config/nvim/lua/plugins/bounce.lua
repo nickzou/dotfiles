cbf5c7cc23acf6b21bf279cf71938c1ccd52f945
Nick Zou <z.nick00@gmail.com>
2025-06-06 21:16:51 -0400

    shorten delay_time

--- nvim/.config/nvim/lua/plugins/bounce.lua
+++ nvim/.config/nvim/lua/plugins/bounce.lua
@@ -2,7 +2,7 @@
     'R1PeR/bounce.nvim',
     config = function()
       require('bounce').setup {
-        delay_time = 1000,
+        delay_time = 300,
         highlight_group_name = '@text.todo',
         more_jumps = false,
         display_mode = "virtual_line",
