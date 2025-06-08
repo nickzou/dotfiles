155a12d8be7883838537d1eb82a979ac64a1a2b3
Nick Zou <z.nick00@gmail.com>
2025-06-08 18:33:51 -0400

    change theme to onedark

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
-return {
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('nordic').load()
-    end
+return {  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('onedark').setup {
+      style = 'darker'
+    }
+    -- Enable theme
+    require('onedark').load()
+  end
 }
