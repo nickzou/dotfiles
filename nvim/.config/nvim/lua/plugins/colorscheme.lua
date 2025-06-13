99ceb239e49f53465db1fbdf091f7191199a70d7
Nick Zou <z.nick00@gmail.com>
2025-06-13 00:16:05 -0400

    make theme background transparent

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,8 @@ return {  "navarasu/onedark.nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require('onedark').setup {
-      style = 'deep'
+      style = 'deep',
+      transparent = true
     }
     -- Enable theme
     require('onedark').load()
