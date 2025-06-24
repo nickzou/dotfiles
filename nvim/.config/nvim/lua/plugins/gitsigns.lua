c01341554b83be15faa4eccd262ab6cc20dbd208
Nick Zou <z.nick00@gmail.com>
2025-06-24 13:42:07 -0400

    update gitsigns config

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -10,9 +10,18 @@ return {
         changedelete = { text = '~' },
       },
       signs_staged_enable = true,
-      linehl = true,
+      linehl = false,
+      numhl = true,
       signcolumn = true,
       current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 500,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
       word_diff  = true,
     })
   end
