2b68c09c43e2357e9e6c427487b1710e577e5616
Nick Zou <z.nick00@gmail.com>
2025-09-27 12:28:52 -0400

    fix live_grep settings

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -5,11 +5,10 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
-	  grep = {
-            rg_opts = "--column --line-number --no-heading --color=always --smart-case --hidden "..
-              "--color=never --hidden -g '!node_modules' -g '!.git' -g '!vendor'"
-	  },
-	})
+      grep = {
+        rg_opts = "--column --line-number --no-heading --color=always --smart-case --hidden -g '!{node_modules,.git,vendor}'"
+      },
+    })
 	vim.keymap.set("n", "<space>p", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "<space>f", "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true })
 	vim.keymap.set("v", "<space>g", "<cmd>lua require('fzf-lua').grep_visual()<CR>", { noremap = true, silent = true })
