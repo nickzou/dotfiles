c46bd37a45b64088c6be55ffcdb73951b4fc10a0
Nick Zou <z.nick00@gmail.com>
2026-05-01 11:44:44 -0400

    add twig

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,13 +2,15 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.autoread = true
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-    pattern = "*",
-    command = "if mode() != 'c' | checktime | endif"
+	pattern = "*",
+	command = "if mode() != 'c' | checktime | endif",
 })
 vim.o.mouse = ""
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.g.PHP_outdentphpescape = 0
+
+vim.filetype.add({ extension = { twig = "twig" } })
 
 return {}
