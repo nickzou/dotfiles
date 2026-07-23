a852435fe45650ef12988b3e153fb5b8e12d365a
Nick Zou <z.nick00@gmail.com>
2026-07-22 22:59:47 -0400

    update default workspaces for messaing apps

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -277,6 +277,27 @@ hl.window_rule({
     workspace = "3",
 })
 
+-- Always open Discord on workspace 10
+hl.window_rule({
+    name      = "discord-workspace-10",
+    match     = { class = "^(discord)$" },
+    workspace = "10",
+})
+
+-- Always open Signal on workspace 10
+hl.window_rule({
+    name      = "signal-workspace-10",
+    match     = { class = "^(signal)$" },
+    workspace = "10",
+})
+
+-- Always open ZapZap on workspace 10
+hl.window_rule({
+    name      = "zapzap-workspace-10",
+    match     = { class = "^(com.rtosta.zapzap)$" },
+    workspace = "10",
+})
+
 -- Fix some dragging issues with XWayland
 hl.window_rule({
     name  = "fix-xwayland-drags",
